3512c04e9f33dce89372d8f0ca22cbad721a3ff6
Cheyoon Jung <jessietwo94@gmail.com>
2022-03-28 10:52:59 +0200

    update

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-'use strict'
+"use strict";
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const island = $("#island");
+const villagers = $("#villagers");
+const encyclopedia = $("#encyclopedia");
+const collection = $("#collection");
+
+///////////////////////////
+// temporary hide
+window.addEventListener("load", function () {
+  island.classList.add("hide");
+  encyclopedia.classList.add("hide");
+  collection.classList.add("hide");
+});
+////////////////////////////
